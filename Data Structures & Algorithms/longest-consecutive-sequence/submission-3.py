1dd27febb5cef421be8a67f961fc77850040e26b
Atharva Gupta <62479753+atharva789@users.noreply.github.com>
2026-04-08 20:19:36 -0400

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        hashset = set()
+        for val in nums:
+            if val in hashset:
+                continue
+            hashset.add(val)
+        seq_starts = {}
+        largest_seq = float('-inf')
+        for val in hashset:
+            if val-1 not in hashset:
+                seq_starts[val] = []
+            else:
+                # loop thru keys? RETARDED
+                smallest_delta = float('inf')
+                for start_num in seq_starts:
+                    delta = val - start_num
+                    if delta < smallest_delta:
+                        smallest_delta = delta
+                key = val - smallest_delta
+
+                seq_starts[key].append(val)
+                largest_seq = len(seq_starts[key]) if len(seq_starts[key]) > largest_seq else largest_seq
+        if largest_seq == float('-inf'):
+            return 1
+        return int(largest_seq)+1
+        
